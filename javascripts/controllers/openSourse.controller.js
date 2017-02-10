ead8f0c47160fb354bf14e63894a09395ea1ad05
MarxJiao <marxjiao@gmail.com>
2017-02-10 15:36:11 +0800

    all done

--- javascripts/controllers/openSourse.controller.js
+++ javascripts/controllers/openSourse.controller.js
@@ -16,7 +16,7 @@ marxBlog.controller('openSourseCtrl', function ($scope, $timeout, $mdSidenav, $l
             time: '2016 年 11 月 ',
             introduction: '为Vue封装的iscroll指令。解决了在Vue项目中iscroll直接操作dom的问题，实现了指令跨钩子共享数据和多个指令间的数据隔离。项目已上传为npm包，方便其它开发者直接使用。',
             link: 'https://github.com/MarxJiao/vue-iscroll-directive',
-            tags: ['vue指令', 'npm']
+            tags: ['npm', 'vue指令']
         },
         {
             name: '前端代码规范检查插件',
@@ -26,11 +26,11 @@ marxBlog.controller('openSourseCtrl', function ($scope, $timeout, $mdSidenav, $l
             tags: ['node.js', 'typescript']
         },
         {
-            name: 'jQuery日期插件',
-            time: '2015 年 11 月 ',
-            introduction: '一个简单的jQuery日期插件，js代码只有400多行，注释占一半，思路清晰易懂，方便使用者进行二次开发，定制功能。100行css方便定制样式。',
-            link: 'https://github.com/MarxJiao/date-picker',
-            tags: ['jQuery插件', '面向对象']
+            name: '修复material-components问题',
+            time: '2016 年 8 月 ',
+            introduction: 'material-components是使用typescript开发的基于material design的vue组件库。为项目修复tab指针不正确问题。',
+            link: 'https://github.com/appcomponents/material-components',
+            tags: ['typescript', 'vue']
         }
     ];
 });
